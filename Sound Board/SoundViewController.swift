244c5707eea3c4c7f1e8b669e9ac9cb2c0ffb538
Mark Krawczuk <mkrawczuk@tw-mbp-mkrawczuk.attlocal.net>
2017-01-10 22:35:00 -0800

    set up UI, finished lecture 87

---
+++ Sound Board/SoundViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SoundViewController.swift
+//  Sound Board
+//
+//  Created by Mark Krawczuk on 1/10/17.
+//  Copyright © 2017 Mark Krawczuk. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    
+    @IBOutlet weak var recordButton: UIButton!
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+
+    @IBAction func playTapped(_ sender: Any) {
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+    
+}
